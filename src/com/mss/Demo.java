31cc9f2425babe98a338f907fd2d4786d19aef59
ksivasaikumar <skanubojji@miraclesoft.com>
2016-06-14 20:22:39 +0530

    my commit

--- src/com/mss/Demo.java
+++ src/com/mss/Demo.java
@@ -1,9 +1,19 @@
 package com.mss;
 
+import java.util.Scanner;
+
 public class Demo {
 	public static void main(String args[])
 	{
 		System.out.println("hello world");
+		int n;
+		Scanner scan=new Scanner(System.in);
+		System.out.println("enter the value");
+		n=scan.nextInt();
+		for(int i=0;i<=n;i++)
+		{
+			System.out.println(n+"*"+i+"="+(n*i));
+		}
 	}
 
 }
